7cf13ef690d84b8a69e2104e0d0d75234a6a9ded
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-21 11:43:34 +0000

    Updates

---
+++ precache-manifest.62cbdc1ba53793633f074b353869c405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e53e06b53b34eb1400fe3ae48509aeb",
+    "url": "/dashboardTemperature/index.html"
+  },
+  {
+    "revision": "b60ccfd70e6ef3969ff6",
+    "url": "/dashboardTemperature/static/css/main.d337b5b7.chunk.css"
+  },
+  {
+    "revision": "0b09df63b5647dd35d6f",
+    "url": "/dashboardTemperature/static/js/2.3d4269d1.chunk.js"
+  },
+  {
+    "revision": "b60ccfd70e6ef3969ff6",
+    "url": "/dashboardTemperature/static/js/main.cea21d96.chunk.js"
+  },
+  {
+    "revision": "44ad688a4ec108b30550",
+    "url": "/dashboardTemperature/static/js/runtime-main.29e13212.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dashboardTemperature/static/media/logo.25bf045c.svg"
+  }
+]);
